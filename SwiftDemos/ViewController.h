885603c73a2dad64ce3c0455eb01609277f0ae77
EG <fangweiyi@gdu-tech.com>
2017-05-10 16:42:57 +0800

    initial

---
+++ SwiftDemos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwiftDemos
+//
+//  Created by EG on 2017/5/10.
+//  Copyright © 2017年 EGMade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwiftDemos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwiftDemos
+//
+//  Created by EG on 2017/5/10.
+//  Copyright © 2017年 EGMade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
